bcb2e23611748f4c70d5416f806d6f5c2db10bab
Pradyumna Sridhara <prsridha@ucsd.edu>
2022-02-14 17:41:29 -0800

    added code from previous version

---
+++ src/assignment2.py
@@ -0,0 +1,340 @@
+import os
+import pyspark.sql.functions as F
+import pyspark.sql.types as T
+from utilities import SEED
+# import any other dependencies you want, but make sure only to use the ones
+# availiable on AWS EMR
+
+# ---------------- choose input format, dataframe or rdd ----------------------
+INPUT_FORMAT = 'dataframe'  # change to 'rdd' if you wish to use rdd inputs
+# -----------------------------------------------------------------------------
+if INPUT_FORMAT == 'dataframe':
+    import pyspark.ml as M
+    import pyspark.sql.functions as F
+    import pyspark.sql.types as T
+    from pyspark.ml.regression import DecisionTreeRegressor
+    from pyspark.ml.evaluation import RegressionEvaluator
+if INPUT_FORMAT == 'koalas':
+    import databricks.koalas as ks
+elif INPUT_FORMAT == 'rdd':
+    import pyspark.mllib as M
+    from pyspark.mllib.feature import Word2Vec
+    from pyspark.mllib.linalg import Vectors
+    from pyspark.mllib.linalg.distributed import RowMatrix
+    from pyspark.mllib.tree import DecisionTree
+    from pyspark.mllib.regression import LabeledPoint
+    from pyspark.mllib.linalg import DenseVector
+    from pyspark.mllib.evaluation import RegressionMetrics
+
+
+# ---------- Begin definition of helper functions, if you need any ------------
+
+# def task_1_helper():
+#   pass
+
+# -----------------------------------------------------------------------------
+
+
+def task_1(data_io, review_data, product_data):
+    # -----------------------------Column names--------------------------------
+    # Inputs:
+    asin_column = 'asin'
+    overall_column = 'overall'
+    # Outputs:
+    mean_rating_column = 'meanRating'
+    count_rating_column = 'countRating'
+    # -------------------------------------------------------------------------
+
+    # ---------------------- Your implementation begins------------------------
+
+
+
+
+
+    # -------------------------------------------------------------------------
+
+    # ---------------------- Put results in res dict --------------------------
+    # Calculate the values programmaticly. Do not change the keys and do not
+    # hard-code values in the dict. Your submission will be evaluated with
+    # different inputs.
+    # Modify the values of the following dictionary accordingly.
+    res = {
+        'count_total': None,
+        'mean_meanRating': None,
+        'variance_meanRating': None,
+        'numNulls_meanRating': None,
+        'mean_countRating': None,
+        'variance_countRating': None,
+        'numNulls_countRating': None
+    }
+    # Modify res:
+
+
+
+
+    # -------------------------------------------------------------------------
+
+    # ----------------------------- Do not change -----------------------------
+    data_io.save(res, 'task_1')
+    return res
+    # -------------------------------------------------------------------------
+
+
+def task_2(data_io, product_data):
+    # -----------------------------Column names--------------------------------
+    # Inputs:
+    salesRank_column = 'salesRank'
+    categories_column = 'categories'
+    asin_column = 'asin'
+    # Outputs:
+    category_column = 'category'
+    bestSalesCategory_column = 'bestSalesCategory'
+    bestSalesRank_column = 'bestSalesRank'
+    # -------------------------------------------------------------------------
+
+    # ---------------------- Your implementation begins------------------------
+
+
+
+
+
+    # -------------------------------------------------------------------------
+
+    # ---------------------- Put results in res dict --------------------------
+    res = {
+        'count_total': None,
+        'mean_bestSalesRank': None,
+        'variance_bestSalesRank': None,
+        'numNulls_category': None,
+        'countDistinct_category': None,
+        'numNulls_bestSalesCategory': None,
+        'countDistinct_bestSalesCategory': None
+    }
+    # Modify res:
+
+
+
+
+    # -------------------------------------------------------------------------
+
+    # ----------------------------- Do not change -----------------------------
+    data_io.save(res, 'task_2')
+    return res
+    # -------------------------------------------------------------------------
+
+
+def task_3(data_io, product_data):
+    # -----------------------------Column names--------------------------------
+    # Inputs:
+    asin_column = 'asin'
+    price_column = 'price'
+    attribute = 'also_viewed'
+    related_column = 'related'
+    # Outputs:
+    meanPriceAlsoViewed_column = 'meanPriceAlsoViewed'
+    countAlsoViewed_column = 'countAlsoViewed'
+    # -------------------------------------------------------------------------
+
+    # ---------------------- Your implementation begins------------------------
+
+
+
+
+
+    # -------------------------------------------------------------------------
+
+    # ---------------------- Put results in res dict --------------------------
+    res = {
+        'count_total': None,
+        'mean_meanPriceAlsoViewed': None,
+        'variance_meanPriceAlsoViewed': None,
+        'numNulls_meanPriceAlsoViewed': None,
+        'mean_countAlsoViewed': None,
+        'variance_countAlsoViewed': None,
+        'numNulls_countAlsoViewed': None
+    }
+    # Modify res:
+
+
+
+
+    # -------------------------------------------------------------------------
+
+    # ----------------------------- Do not change -----------------------------
+    data_io.save(res, 'task_3')
+    return res
+    # -------------------------------------------------------------------------
+
+
+def task_4(data_io, product_data):
+    # -----------------------------Column names--------------------------------
+    # Inputs:
+    price_column = 'price'
+    title_column = 'title'
+    # Outputs:
+    meanImputedPrice_column = 'meanImputedPrice'
+    medianImputedPrice_column = 'medianImputedPrice'
+    unknownImputedTitle_column = 'unknownImputedTitle'
+    # -------------------------------------------------------------------------
+
+    # ---------------------- Your implementation begins------------------------
+
+
+
+
+
+    # -------------------------------------------------------------------------
+
+    # ---------------------- Put results in res dict --------------------------
+    res = {
+        'count_total': None,
+        'mean_meanImputedPrice': None,
+        'variance_meanImputedPrice': None,
+        'numNulls_meanImputedPrice': None,
+        'mean_medianImputedPrice': None,
+        'variance_medianImputedPrice': None,
+        'numNulls_medianImputedPrice': None,
+        'numUnknowns_unknownImputedTitle': None
+    }
+    # Modify res:
+
+
+
+
+    # -------------------------------------------------------------------------
+
+    # ----------------------------- Do not change -----------------------------
+    data_io.save(res, 'task_4')
+    return res
+    # -------------------------------------------------------------------------
+
+
+def task_5(data_io, product_processed_data, word_0, word_1, word_2):
+    # -----------------------------Column names--------------------------------
+    # Inputs:
+    title_column = 'title'
+    # Outputs:
+    titleArray_column = 'titleArray'
+    titleVector_column = 'titleVector'
+    # -------------------------------------------------------------------------
+
+    # ---------------------- Your implementation begins------------------------
+
+
+
+
+
+    # -------------------------------------------------------------------------
+
+    # ---------------------- Put results in res dict --------------------------
+    res = {
+        'count_total': None,
+        'size_vocabulary': None,
+        'word_0_synonyms': [(None, None), ],
+        'word_1_synonyms': [(None, None), ],
+        'word_2_synonyms': [(None, None), ]
+    }
+    # Modify res:
+
+
+
+    # -------------------------------------------------------------------------
+
+    # ----------------------------- Do not change -----------------------------
+    data_io.save(res, 'task_5')
+    return res
+    # -------------------------------------------------------------------------
+
+
+def task_6(data_io, product_processed_data):
+    # -----------------------------Column names--------------------------------
+    # Inputs:
+    category_column = 'category'
+    # Outputs:
+    categoryIndex_column = 'categoryIndex'
+    categoryOneHot_column = 'categoryOneHot'
+    categoryPCA_column = 'categoryPCA'
+    # -------------------------------------------------------------------------    
+
+    # ---------------------- Your implementation begins------------------------
+
+
+
+
+
+    # -------------------------------------------------------------------------
+
+    # ---------------------- Put results in res dict --------------------------
+    res = {
+        'count_total': None,
+        'meanVector_categoryOneHot': [None, ],
+        'meanVector_categoryPCA': [None, ]
+    }
+    # Modify res:
+
+
+
+
+    # -------------------------------------------------------------------------
+
+    # ----------------------------- Do not change -----------------------------
+    data_io.save(res, 'task_6')
+    return res
+    # -------------------------------------------------------------------------
+    
+    
+def task_7(data_io, train_data, test_data):
+    
+    # ---------------------- Your implementation begins------------------------
+    
+    
+    
+    
+    
+    # -------------------------------------------------------------------------
+    
+    
+    # ---------------------- Put results in res dict --------------------------
+    res = {
+        'test_rmse': None
+    }
+    # Modify res:
+
+
+    # -------------------------------------------------------------------------
+
+    # ----------------------------- Do not change -----------------------------
+    data_io.save(res, 'task_7')
+    return res
+    # -------------------------------------------------------------------------
+    
+    
+def task_8(data_io, train_data, test_data):
+    
+    # ---------------------- Your implementation begins------------------------
+    
+    
+    
+    
+    
+    # -------------------------------------------------------------------------
+    
+    
+    # ---------------------- Put results in res dict --------------------------
+    res = {
+        'test_rmse': None,
+        'valid_rmse_depth_5': None,
+        'valid_rmse_depth_7': None,
+        'valid_rmse_depth_9': None,
+        'valid_rmse_depth_12': None,
+    }
+    # Modify res:
+
+
+    # -------------------------------------------------------------------------
+
+    # ----------------------------- Do not change -----------------------------
+    data_io.save(res, 'task_8')
+    return res
+    # -------------------------------------------------------------------------
+
